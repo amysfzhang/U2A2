98204972421cfd04db0e81e7355255ba41a4f902
342905163 <342905163@C-MXL5400ZY1.tdsb.on.ca>
2023-11-08 11:06:01 -0500

    created frame

---
+++ U2A2_AmyZhang/src/main/java/com/mycompany/u2a2_amyzhang/U2A2_AmyZhang.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.u2a2_amyzhang;
+
+/**
+ *
+ * @author 342905163
+ */
+public class U2A2_AmyZhang {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
